8a2dfef1b0c29c457bd283bffb01c5013be8ad43
Seunghwan Lee <raon0505@gmail.com>
2020-08-11 03:02:02 +0900

    Updates

---
+++ precache-manifest.97e104864401fc50bfbda20d37187c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14dfd65647cf97dfe77a6491c887c54",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "4a1b2ba8c76d048f8ab8",
+    "url": "/TodoList/static/css/main.1dff3932.chunk.css"
+  },
+  {
+    "revision": "5414477af7f290adee39",
+    "url": "/TodoList/static/js/2.4ebe40d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TodoList/static/js/2.4ebe40d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1b2ba8c76d048f8ab8",
+    "url": "/TodoList/static/js/main.bda1043f.chunk.js"
+  },
+  {
+    "revision": "e8f0df28d4b304e02d01",
+    "url": "/TodoList/static/js/runtime-main.5c56d921.js"
+  }
+]);
